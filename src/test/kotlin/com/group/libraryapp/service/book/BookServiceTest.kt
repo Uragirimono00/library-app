57a6cdeccb0bbaf52ebdd65db58f1a83e7493780
urgirimono00 <gudtj7578@gmail.com>
2023-09-28 21:09:08 +0900

    Java로 작성된 기존 코드를 모두 Kotlin 코드로 리팩토링

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -78,7 +78,7 @@ class BookServiceTest @Autowired constructor(
         assertThrows<IllegalArgumentException> {
             bookService.loanBook(request)
         }.message.apply {
-            assertThat(this).isEqualTo("진작 대출되어 있는 책입니다")
+            assertThat(this).isEqualTo("진작 대출되어 있는 책입니다.")
         }
     }
 
